b6cbe1659c9d3b8bddb52e30c110174b70e21159
andrewyewcy <93403701+andrewyewcy@users.noreply.github.com>
2021-12-08 16:32:35 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring available data from the Foundation GraphQL API to build a model that predicts the price of new Non-fungible Tokens (NFTs)',
+    author='Andrew Yew',
+    license='MIT',
+)
